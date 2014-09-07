fef90dfdfa1894fd88dacf513190b5f76f26865d
rnparks <rnparks@gmail.com>
2014-09-07 12:58:21 -0400

    working search bar

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source "https://rubygems.org"
+
+ruby "2.1.2"
+
+gem 'sinatra', '1.4.5'
+gem 'redis',  '3.1.0'
+gem 'httparty'
+
+# only used in development locally
+group :development do
+  gem 'shotgun'
+end
+
+group :production do
+  # gems specific just in the production environment
+end
+
+group :test do
+  gem 'rspec'
+end
+
+group :development, :test do
+  gem 'pry'
+end
---
+++ app.rb
@@ -0,0 +1,88 @@
+require 'sinatra/base'
+require 'httparty'
+require 'securerandom'
+require 'pry'
+require 'json'
+
+class App < Sinatra::Base
+
+  ########################
+  # Configuration
+  ########################
+
+  configure do
+    enable :logging
+    enable :method_override
+    enable :sessions
+    set :session_secret, 'super secret'
+    CLIENT_ID = "447724783620-c6vi7jn7cge9dn1n2p145vvfpjkke4oq.apps.googleusercontent.com"
+    EMAIL_ADDRESS = "447724783620-c6vi7jn7cge9dn1n2p145vvfpjkke4oq@developer.gserviceaccount.com"
+    CLIENT_SECRET = "40OqiWZWRzcA1cPt55eaTA09"
+    REDIRECT_URIS = "http://127.0.0.1:9292/oauth2callback"
+    JAVASCRIPT_ORIGINS = "none"
+  end
+
+  before do
+    logger.info "Request Headers: #{headers}"
+    logger.warn "Params: #{params}"
+  end
+
+  after do
+    logger.info "Response Headers: #{response.headers}"
+  end
+
+  ########################
+  # Routes
+  ########################
+
+  get('/') do
+    state = SecureRandom.urlsafe_base64
+    @google_post = "https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.login&state=#{state}&redirect_uri=#{REDIRECT_URIS}&response_type=code&client_id=#{CLIENT_ID}&access_type=offline"
+    render(:erb, :index, :layout => :login)
+  end
+
+  get('/oauth2callback') do
+    code = params["code"]
+    response = HTTParty.post("https://accounts.google.com/o/oauth2/token",
+      :body => {:code => code,
+                :client_id => CLIENT_ID,
+                :client_secret => CLIENT_SECRET,
+                :redirect_uri => REDIRECT_URIS,
+                :grant_type => "authorization_code"
+      })
+
+    session[:access_token] = response["access_token"]
+    redirect to("/market")
+    end
+
+    get('/market') do
+      data = HTTParty.get("http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op&e=.csv")
+      if session[:access_token] == nil
+        redirect to('/')
+      else
+      render(:erb, :market)
+      end
+    end
+
+    post('/market') do
+      search = params[:search].downcase
+      redirect to("/#{search}")
+    end
+
+
+    get('/goog') do
+      google_quote = HTTParty.get("http://download.finance.yahoo.com/d/quotes.csv?s=GOOG&f=l1.csv")
+      @quote = google_quote.split(",")[0].to_f
+      binding.pry
+      render(:erb, :goog)
+    end
+
+    get('/aapl') do
+      aapl_quote = HTTParty.get("http://download.finance.yahoo.com/d/quotes.csv?s=AAPL&f=l1.csv")
+      @quote = aapl_quote.split(",")[0].to_f
+      render(:erb, :aapl)
+    end
+
+end #class end
+
+
