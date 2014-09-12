b514f39728204ca6a8944cb70e2639860ee3b610
rnparks <rnparks@gmail.com>
2014-09-12 00:20:23 -0400

    correction to password function

--- helpers/ryan_functions.rb
+++ helpers/ryan_functions.rb
@@ -2,8 +2,9 @@
 
 module RyanFunctions
 
-def password?
-  redirect to('/') if session[:access_token_google] == nil
-end
+  def self.password?
+    redirect to('/') if session[:access_token_google] == nil
+  end
 
 end
+
